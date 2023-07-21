04feb2660996526a90c1a7c20691b64577e01b7f
Alireza Khalili <alireza.kh077@yahoo.com>
2023-07-21 16:13:30 +0200

    slack and jira placeholders

--- trw.sh
+++ trw.sh
@@ -1,9 +1,19 @@
-#!/bin/sh -e
+#!/bin/sh 
 
-SLEEPTIME=10
+SLEEP_SEC=10
+
+JIRA_ENABLE=$JIRA_ENABLE
+JIRA_HOST=$JIRA_HOST
+JIRA_USER=$JIRA_USER
+JIRA_PASS=$JIRA_PASS
+
+SLACK_ENABLE=$JIRA_ENABLE
+SLACK_HOST=$JIRA_HOST
+SLACK_USER=$JIRA_USER
+SLACK_PASS=$JIRA_PASS
 
 is_processed() {
-    grep -q "$1" ./processed_requests.txt
+	grep -q "$1" ./processed_requests.txt 2> /dev/null
 }
 
 while true; do
@@ -17,23 +27,30 @@ while true; do
 		for line in "${lines[@]}"; do
 			echo "-----------------------------"
 			items=($line)
-			uuid=${items[0]}   # UUID
-            reviewer=${items[1]}   # Reviewer
-            role=${items[2]}   # Roles
-            reason=${items[3]}   # Reason
+			uuid=${items[0]}
+			reviewer=${items[1]}
+			role=${items[2]}
+			reason=${items[3]}
 			if is_processed "$uuid"; then
-                echo "Request with UUID $uuid already processed. Skipping..."
-            else
-                echo "Processing new request with UUID $uuid..."
-                # Create a message here to be Slack Payload
-				# Even Jira Ticket can be created too
-                echo "$uuid" >> processed_requests.txt
-            fi
+				echo "Request with UUID $uuid already processed. Skipping..."
+			else
+				echo "Processing new request with UUID $uuid..."
+				if [[ "$JIRA_ENABLE" == "true" ]]; then
+					# Even Jira Ticket can be created too
+					echo "jira here"
+				fi
+				if [[ "$SLACK_ENABLE" == "true" ]]; then
+					# Create a message here to be Slack Payload
+					echo "slack here"
+				fi
+				echo "$uuid" >> processed_requests.txt
+			fi
 		done
 		echo "============================="
-		sleep $SLEEPTIME
+		sleep $SLEEP_SEC
 	else
 		echo "[INFO] No Pending Request Observed"
-		sleep $SLEEPTIME
+		echo "----------------------------------"
+		sleep $SLEEP_SEC
 	fi
 done
